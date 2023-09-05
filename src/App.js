fde7cbbc068aee9eb41c213fbf99ec26a44411b0
wkzhiro <wkzhiro0116@yahoo.co.jp>
2023-09-05 09:24:15 +0900

    2nd

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { useState } from "react";
 import axios from "axios";
 
 const App = () => {  
-  const url = "http://127.0.0.1:8000/";
+  const url = "https://webapp-class-4-1.azurewebsites.net/";
   const [buyproducts, setProducts] = useState([]);
   const [product, setProduct] = useState({productcode:"",NAME:"", PRICE: ""});
   // const [totalprice, setTotalprice]=useState(0);
